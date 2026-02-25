eb23d1fcae32f84193bac389739cea9e5ed655b7
Sergio Herreros <202113370@alu.comillas.edu>
2026-02-25 12:13:37 +0100

    Binary forward reward

--- src/environment.py
+++ src/environment.py
@@ -32,10 +32,9 @@ class CustomReward(gym.Wrapper):
         else:
             pos_after = float(self.unwrapped.data.qpos[0])
 
-        dt = self.unwrapped.dt
-        forward_reward = (
-            self.unwrapped._forward_reward_weight * (pos_after - pos_before) / dt
-        )
+        x_vel = (pos_after - pos_before) / self.unwrapped.dt
+        # forward_reward = self.unwrapped._forward_reward_weight * x_vel
+        forward_reward = (x_vel >= 0) * 1.0
         ctrl_cost = self.unwrapped._ctrl_cost_weight * float(np.sum(np.square(action)))
         healthy_reward = self.unwrapped._healthy_reward if not terminated else 0.0
 
